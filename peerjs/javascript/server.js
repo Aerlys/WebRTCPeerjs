35e309e1fae9a5c25f7e958e8990e0c80ad537b1
Aerlys <143600@supinfo.com>
2014-11-18 09:52:03 +0100

    Express integration

--- peerjs/javascript/server.js
+++ peerjs/javascript/server.js
@@ -1,11 +1,23 @@
-var http = require('http');
-var PeerServer = require('peer').PeerServer;
-var server = PeerServer({port: 9000, path: '/myapp'});
+var express = require('express');
+var app = express();
+var ExpressPeerServer = require('peer').ExpressPeerServer;
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin' : '*'
-    });
-    response.end('Hello World\n');
-}).listen(1337);
+app.get('/myapp', function(req, res, next) { 
+	res.header('Access-Control-Allow-Origin', '*');
+	res.send('Hello world!'); });
+
+var server = require('http').createServer(app);
+
+var options = {
+    debug: true
+}
+
+app.use('/myapp', ExpressPeerServer(server, options));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+server.listen(9000);
